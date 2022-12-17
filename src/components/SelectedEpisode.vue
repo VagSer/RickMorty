9d8c748a8886d1e98cb3622bf6a801a7469364a0
Sergey Vagaytsev <thevagser@gmail.com>
2022-12-17 12:34:32 +0700

    От эпизода можно перейти к персонажам

--- src/components/SelectedEpisode.vue
+++ src/components/SelectedEpisode.vue
@@ -5,7 +5,9 @@
             <p><strong>Эпизод: </strong>{{selectedItem.episode}}</p>
             <p><strong>Персонажи: </strong>
             <ul>
-                <li v-for="character in selectedItem.characters">{{character}}</li>
+                <li v-for="character in selectedItem.characters">
+                    <p @click="getMoreInfo(character)">{{character}}</p>
+                </li>
             </ul>
         </p>
     </div>
@@ -13,6 +15,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import axios from 'axios'
 
 export default defineComponent({
     name: 'selected-episode',
@@ -20,6 +23,18 @@ export default defineComponent({
         selectedItem: {
             type: Object,
             required: true
+        }
+    },
+    data() {
+        return {
+            newSelect: {}
+        }
+    },
+    methods: {
+        async getMoreInfo(character: string){
+            const response = await axios.get(`${character}`)
+            this.newSelect = await response.data
+            this.$emit('update', this.newSelect)
         }
     }
 })
