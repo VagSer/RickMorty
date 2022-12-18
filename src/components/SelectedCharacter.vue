36167cd68716b24b36eb64e8f1faf7e399c90077
Sergey Vagaytsev <thevagser@gmail.com>
2022-12-18 22:32:10 +0700

    Правки по стилю

--- src/components/SelectedCharacter.vue
+++ src/components/SelectedCharacter.vue
@@ -1,26 +1,23 @@
 <template>
-    <div>
-        <h3>{{selectedItem.name}}</h3>
-            <img :src="selectedItem.image" :alt="selectedItem.name" id="Item__Image"/>
-            <p><strong>Статус: </strong>{{selectedItem.status}}</p>
-            <p><strong>Вид: </strong>{{selectedItem.species}}</p>
-            <p><strong>Пол: </strong>{{selectedItem.gender}}</p>
-            <p><strong>Текущее местоположение: </strong>
-                <span v-if="selectedItem.location.name !== 'unknown'" @click="getMoreInfo(selectedItem.location.url)">{{selectedItem.location.name}}</span>
-                <span v-else>{{selectedItem.location.name}}</span>
-            </p>
-            <p><strong>Происхождение: </strong>
-                <span v-if="selectedItem.origin.name !== 'unknown'" @click="getMoreInfo(selectedItem.origin.url)">{{selectedItem.origin.name}}</span>
-                <span v-else>{{selectedItem.origin.name}}</span>
-            </p>
-            <p><strong>Эпизоды: </strong>
-            <ul>
-                <li v-for="episode in selectedItem.episode">
-                    <p @click="getMoreInfo(episode)">{{episode}}</p>
-                </li>
-            </ul>
+        <img :src="selectedItem.image" :alt="selectedItem.name" id="Item__Image"/>
+        <p><strong>Статус: </strong>{{selectedItem.status}}</p>
+        <p><strong>Вид: </strong>{{selectedItem.species}}</p>
+        <p><strong>Пол: </strong>{{selectedItem.gender}}</p>
+        <p><strong>Текущее местоположение: </strong>
+            <span v-if="selectedItem.location.name !== 'unknown'" @click="getMoreInfo(selectedItem.location.url)">{{selectedItem.location.name}}</span>
+            <span v-else>{{selectedItem.location.name}}</span>
         </p>
-    </div>
+        <p><strong>Происхождение: </strong>
+            <span v-if="selectedItem.origin.name !== 'unknown'" @click="getMoreInfo(selectedItem.origin.url)">{{selectedItem.origin.name}}</span>
+            <span v-else>{{selectedItem.origin.name}}</span>
+        </p>
+        <p><strong>Эпизоды: </strong>
+        <ul>
+            <li v-for="episode in selectedItem.episode">
+                <p @click="getMoreInfo(episode)">{{episode}}</p>
+            </li>
+        </ul>
+        </p>
 </template>
 
 <script lang="ts">
@@ -40,7 +37,7 @@ export default defineComponent({
             const response = await axios.get(`${url}`)
             this.newSelect = await response.data
             this.$emit('update', this.newSelect)
-        }
+        },
     }
 })
 </script>
